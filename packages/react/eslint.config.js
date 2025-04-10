151943ec4c373fd9f9915c18ce496a63f3e287fe
--help <mrsquaare@mrsquaare.fr>
2025-04-10 20:10:29 +0200

    build: bump deps, use eslint config packages
    Signed-off-by: --help <mrsquaare@mrsquaare.fr>

--- packages/react/eslint.config.js
+++ packages/react/eslint.config.js
@@ -1,39 +1,16 @@
-import react from "eslint-plugin-react";
-import * as reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
-
-import rootConfig from "../../eslint.config.mjs";
+import config from "@sandwich-ui/eslint-config";
+import configReact from "@sandwich-ui/eslint-config-react";
 
 export default [
   { ignores: ["dist"] },
-  ...rootConfig,
-  {
-    ...react.configs.flat.recommended,
-    ...react.configs.flat["jsx-runtime"],
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
+  ...config,
+  ...configReact,
+  /* {
     rules: {
-      "react/jsx-curly-brace-presence": ["warn", { props: "always" }],
-      "react/jsx-sort-props": ["warn"],
-      ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
     },
-  },
+  }, */
 ];
